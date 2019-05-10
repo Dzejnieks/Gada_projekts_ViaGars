d5c1188c0d7020ada86e07404e2a97622b02fa17
Aksels <aksels.skvorcovs@gmail.com>
2019-05-10 20:27:26 +0300

    Centred view elements with wrappers. set image size.

---
+++ web/false;/18/185b3dee13ac1235ecb00dd5b45567f7848cd4eb4986026ef2f0c311c57c1b12.php
@@ -0,0 +1,178 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/viagars/templates/layout/page.html.twig */
+class __TwigTemplate_70b220eca178ec89e649db104b8c1f2a269e5c6fe28bfc8840a7e063ffc7eb5d extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 47];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 47
+        echo "  ";
+        if ($this->getAttribute(($context["page"] ?? null), "header", [])) {
+            // line 48
+            echo "    <header role=\"banner\" class=\"header layout\" id=\"header\">
+      <div class=\"container\">
+          ";
+            // line 50
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "header", [])), "html", null, true);
+            echo "
+      </div>
+    </header>
+  ";
+        }
+        // line 54
+        echo "  <main role=\"main\" class=\"main\" id=\"main\">
+    ";
+        // line 55
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "content", [])), "html", null, true);
+        echo "
+  </main>
+  <footer role=\"contentinfo\" class=\"container\">
+    ";
+        // line 58
+        if ($this->getAttribute(($context["page"] ?? null), "footer", [])) {
+            // line 59
+            echo "      ";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "footer", [])), "html", null, true);
+            echo "
+    ";
+        }
+        // line 61
+        echo "  </footer>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/viagars/templates/layout/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  86 => 61,  80 => 59,  78 => 58,  72 => 55,  69 => 54,  62 => 50,  58 => 48,  55 => 47,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override to display a single page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.html.twig template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - base_path: The base URL path of the Drupal installation. Will usually be
+ *   \"/\" unless you have installed Drupal in a sub-directory.
+ * - is_front: A flag indicating if the current page is the front page.
+ * - logged_in: A flag indicating if the user is registered and signed in.
+ * - is_admin: A flag indicating if the user has permission to access
+ *   administration pages.
+ *
+ * Site identity:
+ * - front_page: The URL of the front page. Use this instead of base_path when
+ *   linking to the front page. This includes the language domain or prefix.
+ *
+ * Page content (in order of occurrence in the default page.html.twig):
+ * - messages: Status and error messages. Should be displayed prominently.
+ * - node: Fully loaded node, if there is an automatically-loaded node
+ *   associated with the page and the node ID is the second argument in the
+ *   page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - page.header: Items for the header region.
+ * - page.primary_menu: Items for the primary menu region.
+ * - page.secondary_menu: Items for the secondary menu region.
+ * - page.hero: Items for the hero content region.
+ * - page.highlighted: Items for the highlighted content region.
+ * - page.help: Dynamic help text, mostly for admin pages.
+ * - page.content: The main content of the current page.
+ * - page.sidebar_first: Items for the first sidebar.
+ * - page.sidebar_second: Items for the second sidebar.
+ * - page.footer: Items for the footer region.
+ * - page.breadcrumb: Items for the breadcrumb region.
+ *
+ * @see template_preprocess_page()
+ * @see html.html.twig
+ */
+#}
+  {% if page.header %}
+    <header role=\"banner\" class=\"header layout\" id=\"header\">
+      <div class=\"container\">
+          {{ page.header }}
+      </div>
+    </header>
+  {% endif %}
+  <main role=\"main\" class=\"main\" id=\"main\">
+    {{ page.content }}
+  </main>
+  <footer role=\"contentinfo\" class=\"container\">
+    {% if page.footer %}
+      {{ page.footer }}
+    {% endif %}
+  </footer>
+", "themes/custom/viagars/templates/layout/page.html.twig", "/app/web/themes/custom/viagars/templates/layout/page.html.twig");
+    }
+}
